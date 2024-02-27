e53d8a2e6f3878694e5d072caa0717f82fe12960
Justin Smith <jmsjustin@gmail.com>
2024-02-26 19:51:17 -0500

    added db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.create(name: "Socks", price: 7.99, image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHcnXKt-hFFYd_MndD4rFR9gThYEAghKuDgg&usqp=CAU", description: "Fuzzy things you put on your feet")
